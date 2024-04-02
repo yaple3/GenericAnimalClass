042bf7da379daa0406eec6e1a831615d00c58142
yaple3 <yaple3@JB204-L26-W-D>
2024-04-02 01:51:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GenericAnimalClass
+{
+
+    // define a generics class named Animal
+    public class Animal<T>
+    {
+        // define a field of type T 
+        public T data;
+
+        // define a constructor of the Animal class 
+        public Animal(T data)
+        {
+            this.data = data;
+            //Console.WriteLine("Data passed: " + this.data);
+        }
+        public T getAnimal()
+        {
+            return data;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Animal<string> animalName = new("Tony the Tiger");
+
+            Animal<int> averageLifeSpan = new(15);
+
+            Animal<string> animalHabitat = new("forests, grasslands, and swamps");
+
+            Animal<bool> endangered = new(true);
+
+            Animal<bool> extinct = new(false);
+
+            Console.WriteLine($"Animal Details: \nName: {animalName.getAnimal()}\nLifespan: {averageLifeSpan.getAnimal()} years\nHabitat: {animalHabitat.getAnimal()}\nEndangered: {endangered.getAnimal()}\nExtinct: {extinct.getAnimal()}");
+        }
+    }
+}
